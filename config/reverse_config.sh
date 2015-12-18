caf8133c674418eefa77674a5972355c2f8a6e4f
Aazhar <achraf.azhar@inria.fr>
2015-12-18 17:22:57 +0100

    Missing update for previous commit.

--- config/reverse_config.sh
+++ config/reverse_config.sh
@@ -4,14 +4,12 @@
 mkdir -p local
 
 # saving local config files
-cp ../anhalytics-frontend/src/main/webapp/js/resource/config.js local/
 cp ../anhalytics-annotate/src/main/resources/annotate.properties local/
 cp ../anhalytics-commons/src/main/resources/commons.properties local/
 cp ../anhalytics-harvest/src/main/resources/harvest.properties local/
 cp ../anhalytics-index/src/main/resources/index.properties local/
 
 # writing generic config files
-cp generic/config.js ../anhalytics-frontend/src/main/webapp/js/resource/
 cp generic/annotate.properties ../anhalytics-annotate/src/main/resources/
 cp generic/commons.properties ../anhalytics-commons/src/main/resources/
 cp generic/harvest.properties ../anhalytics-harvest/src/main/resources/
--- config/update_config.sh
+++ config/update_config.sh
@@ -3,14 +3,12 @@
 mkdir -p generic
 
 # saving generic config files
-cp ../anhalytics-frontend/src/main/webapp/js/resource/config.js generic/
 cp ../anhalytics-annotate/src/main/resources/annotate.properties generic/
 cp ../anhalytics-commons/src/main/resources/commons.properties generic/
 cp ../anhalytics-harvest/src/main/resources/harvest.properties generic/
 cp ../anhalytics-index/src/main/resources/index.properties generic/
 
 # writing local config files
-cp local/config.js ../anhalytics-frontend/src/main/webapp/js/resource/
 cp local/annotate.properties ../anhalytics-annotate/src/main/resources/
 cp local/commons.properties ../anhalytics-commons/src/main/resources/
 cp local/harvest.properties ../anhalytics-harvest/src/main/resources/
