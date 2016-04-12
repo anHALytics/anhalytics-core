c34825afd4bd72251665fa277bbef8dbb13a2926
Azhar <achraf.azhar@inria.fr>
2016-04-12 11:27:04 +0200

    Update config update scripts.

--- config/reverse_config.sh
+++ config/reverse_config.sh
@@ -3,14 +3,10 @@
 
 mkdir -p local
 
-# saving local config files
-cp ../anhalytics-annotate/annotate.properties local/
-cp ../anhalytics-commons/commons.properties local/
-cp ../anhalytics-harvest/harvest.properties local/
-cp ../anhalytics-index/index.properties local/
-
 # writing generic config files
-cp generic/annotate.properties ../anhalytics-annotate/
-cp generic/commons.properties ../anhalytics-commons/
-cp generic/harvest.properties ../anhalytics-harvest/
-cp generic/index.properties ../anhalytics-index/
+cp generic/annotate.properties local/
+cp generic/commons.properties local/
+cp generic/harvest.properties local/
+cp generic/index.properties local/
+cp generic/ingest.properties local/
+cp generic/test.properties local/
--- config/update_config.sh
+++ config/update_config.sh
@@ -2,14 +2,10 @@
 # script for replacing generic config files by local ones (local config files must be up-to-date!)
 mkdir -p generic
 
-# saving generic config files
-cp ../anhalytics-annotate/annotate.properties generic/
-cp ../anhalytics-commons/commons.properties generic/
-cp ../anhalytics-harvest/harvest.properties generic/
-cp ../anhalytics-index/index.properties generic/
-
 # writing local config files
-cp local/annotate.properties ../anhalytics-annotate/
-cp local/commons.properties ../anhalytics-commons/
-cp local/harvest.properties ../anhalytics-harvest/
-cp local/index.properties ../anhalytics-index/
+cp local/annotate.properties generic/
+cp local/commons.properties generic/
+cp local/harvest.properties generic/
+cp local/index.properties generic/
+cp local/ingest.properties generic/
+cp local/test.properties generic/
