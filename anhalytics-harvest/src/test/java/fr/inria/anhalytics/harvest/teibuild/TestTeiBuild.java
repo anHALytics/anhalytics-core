19668e4fa621a3731831358405ab71be235ef8d2
Aazhar <achraf.azhar@inria.fr>
2016-05-15 18:03:48 +0200

    [harvest] Update unit test.

--- anhalytics-harvest/src/test/java/fr/inria/anhalytics/harvest/teibuild/TestTeiBuild.java
+++ anhalytics-harvest/src/test/java/fr/inria/anhalytics/harvest/teibuild/TestTeiBuild.java
@@ -6,18 +6,25 @@ import fr.inria.anhalytics.harvest.properties.HarvestProperties;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringReader;
 import java.util.Iterator;
 import javax.xml.namespace.NamespaceContext;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
 import org.apache.commons.io.FileUtils;
 import org.custommonkey.xmlunit.XMLTestCase;
 import org.junit.Test;
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
 /**
@@ -27,7 +34,7 @@ import org.xml.sax.InputSource;
 public class TestTeiBuild extends XMLTestCase {
 
     @Test
-    public void testTEIMerging() throws Exception {
+    public void testMetadataAppend() throws FileNotFoundException, IOException, XPathExpressionException {
         try {
             HarvestProperties.init("harvest.properties");
         } catch (Exception exp) {
@@ -35,47 +42,91 @@ public class TestTeiBuild extends XMLTestCase {
         }
         XPath xPath = XPathFactory.newInstance().newXPath();
         xPath.setNamespaceContext(new MyNamespaceContext());
-        File mtdTeiFile = new File(this.getResourceDir("src/test/resources/").getAbsoluteFile()
-                + "/hal-01110586v1.tei.xml");
-        if (!mtdTeiFile.exists()) {
+
+        File metadata = null, fullTextFile = null;
+        try {
+            metadata = new File(this.getResourceDir("src/test/resources/").getAbsoluteFile()
+                    + "/hal-00576900.tei.xml");
+            /*if (!metadata.exists()) {
+            throw new Exception("Cannot start test, because test resource folder is not correctly set.");
+        }*/
+            fullTextFile = new File(this.getResourceDir("src/test/resources/").getAbsoluteFile()
+                    + "/hal-00576900.fulltext.tei.xml");
+            /*if (!fullTextFile.exists()) {
             throw new Exception("Cannot start test, because test resource folder is not correctly set.");
         }
-        File fullTextFile = new File(this.getResourceDir("src/test/resources/").getAbsoluteFile()
-                + "/hal-01110586v1.fulltext.tei.xml");
-        if (!fullTextFile.exists()) {
-            throw new Exception("Cannot start test, because test resource folder is not correctly set.");
+             */
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        Document corpusTei = TeiBuilder.createTEICorpus(new FileInputStream(mtdTeiFile));
+        Document corpusTei = TeiBuilder.createTEICorpus(new FileInputStream(metadata));
         String corpusTeiString = Utilities.toString(corpusTei);
-        String result = Utilities.toString(TeiBuilder.addGrobidTeiToTei(corpusTeiString, FileUtils.readFileToString(fullTextFile)));
-        
+        String result = Utilities.toString(TeiBuilder.addGrobidTEIToTEICorpus(corpusTeiString, FileUtils.readFileToString(fullTextFile)));
         // some test here...
-        String expected = FileUtils.readFileToString(new File(this.getResourceDir("src/test/resources/").getAbsoluteFile()
-                + "/hal-01110586v1.final.tei.xml"), "UTF-8");
-        String titleHalTei = (String) xPath.evaluate("/teiHeader/titleStmt/title/text()", new InputSource(new FileInputStream(mtdTeiFile)), XPathConstants.STRING);
-        String titleResult = (String) xPath.evaluate("/teiCorpus/teiHeader/titleStmt/title/text()", new InputSource(new StringReader(result)), XPathConstants.STRING);
+        try {
+            String expected = FileUtils.readFileToString(new File(this.getResourceDir("src/test/resources/").getAbsoluteFile()
+                    + "/hal-00576900.corpus.tei.xml"), "UTF-8");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        docFactory.setValidating(false);
+        //docFactory.setNamespaceAware(true);
+        Document metadataDoc = null;
+        DocumentBuilder docBuilder = null;
+        try {
+            docBuilder = docFactory.newDocumentBuilder();
+            metadataDoc = docBuilder.parse(new FileInputStream(metadata));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Document corpusDoc = null;
+        try {
+            docBuilder = docFactory.newDocumentBuilder();
+            corpusDoc = docBuilder.parse(new ByteArrayInputStream(result.getBytes()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String titleHalTei = (String) xPath.compile("/TEI/text/body/listBibl/biblFull/titleStmt/title").evaluate(metadataDoc, XPathConstants.STRING);
+        String titleResult = (String) xPath.compile("/teiCorpus/teiHeader/fileDesc/titleStmt/title").evaluate(corpusDoc, XPathConstants.STRING);
         assertEquals(titleResult.trim(), titleHalTei.trim());
 
-        mtdTeiFile = new File(this.getResourceDir("src/test/resources/").getAbsoluteFile()
-                + "/hal-01110668v1.tei.xml");
-        if (!mtdTeiFile.exists()) {
-            throw new Exception("Cannot start test, because test resource folder is not correctly set.");
+        try {
+            metadata = new File(this.getResourceDir("src/test/resources/").getAbsoluteFile()
+                    + "/inria-00510267.tei.xml");
+            fullTextFile = new File(this.getResourceDir("src/test/resources/").getAbsoluteFile()
+                    + "/inria-00510267.fulltext.tei.xml");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        fullTextFile = new File(this.getResourceDir("src/test/resources/").getAbsoluteFile()
-                + "/hal-01110668v1.fulltext.tei.xml");
-        if (!fullTextFile.exists()) {
-            throw new Exception("Cannot start test, because test resource folder is not correctly set.");
-        }
-        corpusTei = TeiBuilder.createTEICorpus(new FileInputStream(mtdTeiFile));
+        corpusTei = TeiBuilder.createTEICorpus(new FileInputStream(metadata));
         String corpusTeiStream1 = (Utilities.toString(corpusTei));
-        result = Utilities.toString(TeiBuilder.addGrobidTeiToTei(corpusTeiStream1, FileUtils.readFileToString(fullTextFile)));
+        result = Utilities.toString(TeiBuilder.addGrobidTEIToTEICorpus(corpusTeiStream1, FileUtils.readFileToString(fullTextFile)));
 
-        expected = FileUtils.readFileToString(new File(this.getResourceDir("src/test/resources/").getAbsoluteFile()
-                + "/hal-01110668v1.final.tei.xml"), "UTF-8");
-		//assertXpathExists("/TEI[1]/teiHeader[1]", expected);
+        try {
+            docBuilder = docFactory.newDocumentBuilder();
+            metadataDoc = docBuilder.parse(new FileInputStream(metadata));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            docBuilder = docFactory.newDocumentBuilder();
+            corpusDoc = docBuilder.parse(new ByteArrayInputStream(result.getBytes()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String expected;
+        try {
+            expected = FileUtils.readFileToString(new File(this.getResourceDir("src/test/resources/").getAbsoluteFile()
+                    + "/inria-00510267.corpus.tei.xml"), "UTF-8");
+            //assertXpathExists("/TEI[1]/teiHeader[1]", expected);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        titleResult = (String) xPath.evaluate("/teiHeader/titleStmt/title/text()", new InputSource(new FileInputStream(mtdTeiFile)), XPathConstants.STRING);
-        titleHalTei = (String) xPath.evaluate("/teiCorpus/teiHeader/titleStmt/title/text()", new InputSource(new StringReader(result)), XPathConstants.STRING);
+        titleResult = (String) xPath.compile("/TEI/text/body/listBibl/biblFull/titleStmt/title").evaluate(metadataDoc, XPathConstants.STRING);
+        titleHalTei = (String) xPath.compile("/teiCorpus/teiHeader/fileDesc/titleStmt/title").evaluate(corpusDoc, XPathConstants.STRING);
         assertEquals(titleResult.trim(), titleHalTei.trim());
     }
 
