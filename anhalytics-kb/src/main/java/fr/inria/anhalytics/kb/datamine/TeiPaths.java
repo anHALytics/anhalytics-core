5f8c0970e1d27b87f4fafb27df1fbc5235c1121f
Aazhar <achraf.azhar@inria.fr>
2018-02-19 11:46:36 +0100

    Update classCode XPaths for domains and type of document.

--- anhalytics-kb/src/main/java/fr/inria/anhalytics/kb/datamine/TeiPaths.java
+++ anhalytics-kb/src/main/java/fr/inria/anhalytics/kb/datamine/TeiPaths.java
@@ -13,9 +13,9 @@ public interface TeiPaths {
     public final static String IdnoElement = "/teiCorpus/teiHeader/fileDesc/sourceDesc/biblStruct/idno";
     public final static String TitleElement = "/teiCorpus/teiHeader/fileDesc/titleStmt/title";
     public final static String LanguageElement = "/teiCorpus/teiHeader/profileDesc/langUsage/language";
-    public final static String TypologyElement = "/teiCorpus/teiHeader/profileDesc/textClass/classCode[@scheme=\"halTypology\"]";
+    public final static String TypologyElement = "/teiCorpus/teiHeader/profileDesc/textClass/classCode[@scheme=\"typology\"]";
     public final static String SubmissionDateElement = "/teiCorpus/teiHeader/fileDesc/editionStmt/edition[@type=\"current\"]/date[@type=\"whenSubmitted\"]";
-    public final static String DomainElement = "/teiCorpus/teiHeader/profileDesc/textClass/classCode[@scheme=\"halDomain\"]";
+    public final static String DomainElement = "/teiCorpus/teiHeader/profileDesc/textClass/classCode[@scheme=\"domain\"]";
     public final static String EditorElement = "/teiCorpus/teiHeader/fileDesc/sourceDesc/biblStruct/analytic/editor";
     public final static String AuthorElement = "/teiCorpus/teiHeader/fileDesc/sourceDesc/biblStruct/analytic/author";
     
